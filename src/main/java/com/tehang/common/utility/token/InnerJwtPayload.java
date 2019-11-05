0988e0268b0545cf9dea6bec1c15cfa080a9b31f
umasuo <liuquan89@gmail.com>
2019-11-05 18:57:36 +0800

    add jwt support (#1)
    - 添加jwt支持
    - 删除无用文件

---
+++ src/main/java/com/tehang/common/utility/token/InnerJwtPayload.java
@@ -0,0 +1,64 @@
+package com.tehang.common.utility.token;
+
+import com.tehang.common.utility.JsonUtils;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 内部系统中，传递的jwt的payload
+ */
+@Data
+public class InnerJwtPayload implements Serializable {
+
+  private static final long serialVersionUID = -877837145679953510L;
+
+  private Long employeeId;
+  private String clientType;
+  private boolean admin;
+  private Long staffId;
+  private String staffName;
+  private List<String> roles;
+
+  /**
+   * 创建一个新的payload
+   *
+   * @param employeeId
+   * @param clientType
+   * @param isAdmin
+   * @param staffId
+   * @param staffName
+   * @return
+   */
+  public static InnerJwtPayload create(Long employeeId,
+                                       String clientType,
+                                       boolean isAdmin,
+                                       Long staffId,
+                                       String staffName
+  ) {
+    InnerJwtPayload payload = new InnerJwtPayload();
+    payload.employeeId = employeeId;
+    payload.clientType = clientType;
+    payload.admin = isAdmin;
+    payload.staffId = staffId;
+    payload.staffName = staffName;
+    return payload;
+  }
+
+  /**
+   * 从Json字符串转换为对象
+   *
+   * @param payload
+   * @return
+   */
+  public static InnerJwtPayload fromJson(String payload) {
+    return JsonUtils.toClass(payload, InnerJwtPayload.class);
+  }
+
+  @Override
+  public String toString() {
+    return JsonUtils.toJson(this);
+  }
+
+}
---
+++ src/test/groovy/com/tehang/common/utility/token/JwtUtilsTest.groovy
@@ -0,0 +1,35 @@
+package com.tehang.common.utility.token
+
+import com.tehang.common.TestSpecification
+
+class JwtUtilsTest extends TestSpecification {
+
+  InnerJwtPayload payload
+  def secret = "my-secret"
+
+  def setup() {
+    payload = InnerJwtPayload.create(10001L, "IOS", true, 20002L, "tester")
+
+  }
+
+
+  def "test1: create token"() {
+    when:
+
+    def token = JwtUtils.createToken(payload, secret, "tester", "testcase")
+    println(token)
+    then:
+    noExceptionThrown()
+    token != null
+  }
+
+
+  def "test2: fetch payload"() {
+    when:
+    def tokenStr = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ0ZXN0Y2FzZSIsImlzcyI6InRlc3RlciIsImlhdCI6MTU3Mjk0OTQ5MCwiQ3VzdG9tZXJQYXlsb2FkIjoie1wiZW1wbG95ZWVJZFwiOjEwMDAxLFwiY2xpZW50VHlwZVwiOlwiSU9TXCIsXCJhZG1pblwiOnRydWUsXCJzdGFmZklkXCI6MjAwMDIsXCJzdGFmZk5hbWVcIjpcInRlc3RlclwifSJ9.2PI-zfX7l_08UBxUCakaxbhPsgjdqf4IUYmumRBJLg8"
+    def payload1 = JwtUtils.getPayload(tokenStr, secret)
+    then:
+    noExceptionThrown()
+    payload1 == payload
+  }
+}
