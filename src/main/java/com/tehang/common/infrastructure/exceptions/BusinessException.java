68fb019f30d9d3c214061dbd7e6ec7072a6e46ee
Li Jieming <moonkin-ck@qq.com>
2020-07-01 16:36:44 +0800

    Publish 1.1.7

---
+++ src/main/java/com/tehang/common/infrastructure/exceptions/BusinessException.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 MAMCHARGE 深圳智链物联科技有限公司. <http://www.mamcharge.com>
+ *
+ */
+
+package com.tehang.common.infrastructure.exceptions;
+
+/**
+ * 业务异常，可从此异常派生子类来表达不同的业务异常情况，比如密码错误(此类异常可能不是真正的错误，只是表达一种非正常的情况)
+ */
+public class BusinessException extends RuntimeException {
+
+  /**
+   * 无參构造器.
+   */
+  public BusinessException() {
+    super();
+    // do nothing.
+  }
+
+  /**
+   * public constructor with message.
+   *
+   * @param message the message
+   */
+  public BusinessException(String message) {
+    super(message);
+  }
+
+  /**
+   * public constructor with message and cause.
+   *
+   * @param message the message
+   * @param cause   the cause
+   */
+  public BusinessException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
--- src/main/java/com/tehang/common/utility/api/CommonCode.java
+++ src/main/java/com/tehang/common/utility/api/CommonCode.java
@@ -15,9 +15,20 @@ public class CommonCode {
   public static final String SUCCESS_MESSAGE = "OK";
 
   /**
+   * sql执行异常
+   */
+  public static final int SQL_ERROR_CODE = 97;
+
+  /**
+   * sql执行异常信息
+   */
+  public static final String SQL_ERROR_MESSAGE = "内部数据库错误";
+
+  /**
    * 参数错误
    */
   public static final int PARAMETER_ERROR_CODE = 98;
+
   /**
    * 参数错误信息，默认信息.
    */
@@ -27,13 +38,15 @@ public class CommonCode {
    * 其他异常
    */
   public static final int COMMON_ERROR_CODE = 99;
+
   /**
    * 参数错误
    */
-  public static final int UNHANDLER_ERROR_CODE = 500;
+  public static final int UNHANDLED_ERROR_CODE = 500;
+
   /**
    * 参数错误信息，默认信息.
    */
-  public static final String UNHANDLER_ERROR_MESSAGE = "未知错误";
+  public static final String UNHANDLED_ERROR_MESSAGE = "未知错误";
 
 }
