9fb0539076f0623c3fbe5d1684cab6da8fe4fe64
haoyuhai <haoyuhai@163.com>
2020-12-22 09:20:19 +0800

    对于立即加载的缓存，利用CommandLineRunner接口在启动时异步加载 (#21)
    * 对于立即加载的缓存，利用CommandLineRunner接口在启动时异步加载

--- src/main/java/com/tehang/common/utility/event/cache/RefreshableCache.java
+++ src/main/java/com/tehang/common/utility/event/cache/RefreshableCache.java
@@ -1,12 +1,14 @@
 package com.tehang.common.utility.event.cache;
 
+import com.tehang.common.utility.async.AsyncHelper;
 import com.tehang.common.utility.event.DefaultEvent;
 import com.tehang.common.utility.event.DomainEvent;
+import org.springframework.boot.CommandLineRunner;
 
 /**
  * 可动态刷新的缓存接口: 通过订阅对应的事件对缓存进行刷新
  */
-public interface RefreshableCache {
+public interface RefreshableCache extends CommandLineRunner {
 
   /**
    * 刷新缓存
@@ -23,6 +25,25 @@ public interface RefreshableCache {
    */
   default Class<? extends DomainEvent> getEventClass() {
     return DefaultEvent.class;
+  }
+
+  /**
+   * 缓存数据的加载方式：延迟加载 or 提前加载, 默认为延迟加载
+   */
+  default CacheDataFetchType fetchType() {
+    return CacheDataFetchType.LAZY;
+  }
+
+  /**
+   * 应用初始化时，异步加载缓存
+   */
+  default void run(String... args) {
+    if (fetchType() == CacheDataFetchType.EAGER) {
+      AsyncHelper.async(() -> {
+        // 为避免影响应用程序的启动，加载缓存采用异步的方式
+        refresh(null);
+      });
+    }
   }
 
   /**
