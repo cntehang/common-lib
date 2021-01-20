8c12cc287af8c4ef1fe41a002ccab35db5b94362
Li Jieming <moonkin-ck@qq.com>
2021-01-20 10:08:03 +0800

    [功能] NullUtils

---
+++ src/main/java/com/tehang/common/utility/NullUtils.java
@@ -0,0 +1,28 @@
+package com.tehang.common.utility;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class NullUtils {
+  public static <T> T getOrNull(Supplier<T> getMethod) {
+    return getOrElse(getMethod, null);
+  }
+
+  public static <T> T getOrElse(Supplier<T> getMethod, T defaultValue) {
+    return getOrElseGet(getMethod, (ex) -> defaultValue);
+  }
+
+  public static <T> T getOrElseGet(Supplier<T> getMethod, @NotNull Function<NullPointerException, T> defaultGetMethod) {
+    try {
+      return getMethod.get();
+    }
+    catch (NullPointerException exception) {
+      return defaultGetMethod.apply(exception);
+    }
+  }
+}
