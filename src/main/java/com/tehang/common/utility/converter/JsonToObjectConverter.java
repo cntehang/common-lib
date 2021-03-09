6516a149eacaebaa90f2bb9bf5be7dd80837a0af
viper140 <lszs15926284077>
2021-03-09 17:32:28 +0800

    JsonToObjectConverter

---
+++ src/main/java/com/tehang/common/utility/converter/JsonToObjectConverter.java
@@ -0,0 +1,33 @@
+package com.tehang.tmc.services.utility.jpa.converter;
+
+import com.tehang.tmc.services.utility.JsonUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.AttributeConverter;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * 从容应对数据库 null 值，以及传入时候的 null 参数
+ */
+public class JsonToObjectConverter<T> implements AttributeConverter<T, String> {
+
+  @Override
+  public String convertToDatabaseColumn(T attribute) {
+    return attribute == null ? null : JsonUtils.toJson(attribute);
+  }
+
+  @Override
+  public T convertToEntityAttribute(String dbData) {
+    if (StringUtils.isNotBlank(dbData)) {
+      Class<T> clazz = getActualTypeArgument();
+      return JsonUtils.toClass(dbData, clazz);
+
+    } else {
+      return null;
+    }
+  }
+
+  private Class<T> getActualTypeArgument() {
+    return (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+  }
+}
