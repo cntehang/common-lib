99e0923fd1b81c8218b5292b060be000f06c7a8a
haoyuhai <haoyuhai@163.com>
2021-06-27 22:30:34 +0800

    添加ResponseWrapper功能

---
+++ src/main/java/com/tehang/common/utility/rest/annotations/WrapException.java
@@ -0,0 +1,19 @@
+package com.tehang.common.utility.rest.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用于Controller上，将对该Controller中的异常进行包装, 包括该Controller的所有子类
+ */
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface WrapException {
+
+}
