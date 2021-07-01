c6c58da173973e4b1981b1929bfe038681440ee7
ZhangYu <zhangyu@tehang.com>
2021-07-01 16:53:04 +0800

    update forr review

--- src/main/java/com/tehang/common/utility/requestcontextinfo/RequestContextInfoUtils.java
+++ src/main/java/com/tehang/common/utility/requestcontextinfo/RequestContextInfoUtils.java
@@ -1,10 +1,10 @@
 package com.tehang.common.utility.requestcontextinfo;
 
-import com.tehang.common.infrastructure.exceptions.SystemErrorException;
 import com.tehang.common.utility.token.InnerJwtPayload;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * RequestContextInfo工具类
@@ -17,7 +17,7 @@ public final class RequestContextInfoUtils {
    * 获取当前登录员工的id
    */
   public static Long getCurEmployeeId() {
-    return getCurContext().getEmployeeId();
+    return getCurContext() == null ? null : getCurContext().getEmployeeId();
   }
 
   /**
@@ -26,43 +26,34 @@ public final class RequestContextInfoUtils {
    * @return true 若请求来自后台代订人员
    */
   public static boolean isFromAdmin() {
-    return getCurContext().isAdmin();
+    return getCurContext() != null && getCurContext().isAdmin();
   }
 
   /**
    * 获取当前登录的客户端类型
    */
   public static String getCurOperateSource() {
-    String clientType = getCurContext().getClientType();
-
-    return OperateSource.getOperateSource(clientType);
+    return getCurContext() == null ? StringUtils.EMPTY : OperateSource.getOperateSource(getCurContext().getClientType());
   }
 
   /**
    * 获取当前登录后台操作人员的员工id
    */
   public static Long getCurStaffId() {
-    return getCurContext().getStaffId();
+    return getCurContext() == null ? null : getCurContext().getStaffId();
   }
 
   /**
    * 获取当前登录后台操作人员的员工姓名
    */
   public static String getCurStaffName() {
-    return getCurContext().getStaffName();
+    return getCurContext() == null ? StringUtils.EMPTY : getCurContext().getStaffName();
   }
 
   /**
    * 获取上下文
    */
   private static InnerJwtPayload getCurContext() {
-    InnerJwtPayload info = RequestContextInfo.getContext();
-
-    if (info == null) {
-      log.error("InnerJwtPayload 返回空");
-      throw new SystemErrorException("获取请求信息上下文失败");
-    }
-
-    return info;
+    return RequestContextInfo.getContext();
   }
 }
