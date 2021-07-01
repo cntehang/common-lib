69e6263bad963c008ca5386e005f39a3c921de49
ZhangYu <zhangyu@tehang.com>
2021-07-01 16:08:12 +0800

    添加启用DocodeJwt注解

---
+++ src/main/java/com/tehang/common/utility/requestcontextinfo/EnableDecodeJwt.java
@@ -0,0 +1,22 @@
+package com.tehang.common.utility.requestcontextinfo;
+
+import com.tehang.common.infrastructure.filters.DecodeJwtFilter;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 启用DecodeJwt组件
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({JwtConfig.class, DecodeJwtFilter.class})
+public @interface EnableDecodeJwt {
+}
