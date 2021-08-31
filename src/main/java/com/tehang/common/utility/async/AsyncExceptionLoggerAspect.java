58d5bd1437858e204f0ddadce634c4dae3f14aaa
Li Jieming <moonkin-ck@qq.com>
2021-08-31 23:08:45 +0800

    [功能] `@AsyncExceptionLogger` 注解

---
+++ src/main/java/com/tehang/common/utility/async/AsyncExceptionLoggerAspect.java
@@ -0,0 +1,36 @@
+package com.tehang.common.utility.async;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 用来记录异步方法异常的Aspect类
+ */
+@Slf4j
+@Aspect
+@Component
+public class AsyncExceptionLoggerAspect {
+
+  /**
+   * 通过around方式，在目标方法发生异常时记录日志
+   *
+   * @param joinPoint
+   * @return
+   * @throws Throwable
+   */
+  @Around("@annotation(com.tehang.common.utility.async.AsyncExceptionLogger)")
+  public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+    try {
+      return joinPoint.proceed();
+
+    } catch (Exception ex) {
+      log.error("async methods exception happens, msg: {}", ex.getMessage(), ex);
+      throw ex;
+    }
+  }
+}
