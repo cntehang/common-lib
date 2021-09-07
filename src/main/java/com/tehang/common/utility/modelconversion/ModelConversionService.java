0d9d17f5a0a82f3863360ed04ea72a090c35008e
Li Jieming <moonkin-ck@qq.com>
2021-09-08 00:12:34 +0800

    [Fix] ConversionService 乱匹配空字段问题

--- src/main/java/com/tehang/common/utility/modelconversion/ModelConversionService.java
+++ src/main/java/com/tehang/common/utility/modelconversion/ModelConversionService.java
@@ -3,8 +3,11 @@ package com.tehang.common.utility.modelconversion;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * 进行 Model 转换 能够自动对同名属性进行映射（更多实现细节查看 http://modelmapper.org） 如果有特殊的转换需求，通过自定义 `AutoRegisterModelConverter` 以实现
@@ -14,6 +17,11 @@ import org.springframework.stereotype.Service;
 public class ModelConversionService {
 
   private final ModelMapper modelMapper = new ModelMapper();
+
+  @PostConstruct
+  void init() {
+    this.modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+  }
 
   /**
    * convert.
