4e1ddeaf9acdf58aa8fa114dead0603bf171edc5
haoyuhai <haoyuhai@163.com>
2022-11-03 15:00:06 +0800

    添加BjTime.elapsedSeconds方法，方便记录日志时计算花费时间

---
+++ src/main/java/com/tehang/common/utility/time/ElapsedSeconds.java
@@ -0,0 +1,49 @@
+package com.tehang.common.utility.time;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 表示一个时间范围，以秒为单位。常用于日志记录时获取花费的时间。
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ElapsedSeconds implements Serializable {
+
+  private static final long serialVersionUID = -5962799069942105993L;
+
+  private BjTime start;
+  private BjTime end;
+
+  /**
+   * 创建一个时间范围，从指定的开始时间到当前时间为止。
+   */
+  public static ElapsedSeconds from(BjTime start) {
+    return create(start, BjTime.now());
+  }
+
+  public static ElapsedSeconds create(BjTime start, BjTime end) {
+    var result = new ElapsedSeconds();
+    result.start = start;
+    result.end = end;
+    return result;
+  }
+
+  /**
+   * 获取该时间范围的字符串表示，以秒为单位，保留一位小数。
+   */
+  @Override
+  public String toString() {
+    return String.format("%.1f", getSeconds());
+  }
+
+  /**
+   * 获取该时间范围的秒数
+   */
+  public double getSeconds() {
+    return ((double)end.getInnerTime().getMillis() - start.getInnerTime().getMillis()) / 1000;
+  }
+}
