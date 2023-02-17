871038e7120372aea0796714c10266bd951e7fde
haoyuhai <haoyuhai@163.com>
2023-02-17 13:59:28 +0800

    添加解析北京时间的辅助方法

--- src/main/java/com/tehang/common/utility/BjDateUtils.java
+++ src/main/java/com/tehang/common/utility/BjDateUtils.java
@@ -2,12 +2,20 @@ package com.tehang.common.utility;
 
 import com.tehang.common.infrastructure.exceptions.ParameterException;
 import com.tehang.common.utility.time.BjDate;
+import com.tehang.common.utility.time.BjDateTime;
+import com.tehang.common.utility.time.BjTime;
 import com.tehang.common.utility.time.BjTimeToMinute;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.Days;
 import org.joda.time.Minutes;
+
+import static com.tehang.common.utility.time.BjDateTime.ZONE_SHANGHAI;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * BjDate工具类
@@ -18,9 +26,6 @@ public final class BjDateUtils {
 
   /**
    * 获取两个日期之间的间隔天数
-   * @param startDate 开始日期
-   * @param endDate 结束日期
-   * @return 间隔天数
    */
   public static int daysBetween(BjDate startDate, BjDate endDate) {
     if (startDate == null) {
@@ -36,9 +41,6 @@ public final class BjDateUtils {
 
   /**
    * 获取两个时间之间的间隔分钟
-   * @param startTime 开始时间
-   * @param endTime 结束时间
-   * @return 间隔分钟
    */
   public static int minutesBetween(BjTimeToMinute startTime, BjTimeToMinute endTime) {
     if (startTime == null) {
@@ -50,5 +52,20 @@ public final class BjDateUtils {
     }
 
     return Minutes.minutesBetween(startTime.getInnerTime(), endTime.getInnerTime()).getMinutes();
+  }
+
+  /**
+   * 解析utc格式(yyyy-MM-dd'T'HH:mm:ss.SSS'Z')的字符串转为北京时间对象。
+   */
+  public static BjTime parseBjTimeInUtc(String utcTimeString) {
+    if (isBlank(utcTimeString)) {
+      return null;
+    }
+
+    String utcNormalString = StringUtils.replace(utcTimeString, StringUtils.SPACE, "T");
+    DateTime utcTime = new DateTime(utcNormalString, DateTimeZone.UTC);
+    DateTime bjTime = utcTime.toDateTime(DateTimeZone.forID(ZONE_SHANGHAI));
+
+    return BjTime.parse(bjTime.toString(BjDateTime.DATE_FORMAT_TO_MS));
   }
 }
