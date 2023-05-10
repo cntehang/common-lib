49f092b7ea505140151c2cfed784230106e76166
haoyuhai <haoyuhai@163.com>
2023-05-10 14:08:15 +0800

    添加计算年龄的方法

--- src/main/java/com/tehang/common/utility/BjDateUtils.java
+++ src/main/java/com/tehang/common/utility/BjDateUtils.java
@@ -13,6 +13,9 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Days;
 import org.joda.time.Minutes;
+import org.joda.time.Years;
+
+import java.time.Year;
 
 import static com.tehang.common.utility.time.BjDateTime.ZONE_SHANGHAI;
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -52,6 +55,36 @@ public final class BjDateUtils {
     }
 
     return Minutes.minutesBetween(startTime.getInnerTime(), endTime.getInnerTime()).getMinutes();
+  }
+
+  /**
+   * 通过出生日期和当前日期计算年龄（周岁）
+   */
+  public static int calculateAge(BjDate birthday, BjDate currentDate) {
+    if (birthday == null) {
+      throw new ParameterException("出生日期不能为空");
+    }
+    if (currentDate == null) {
+      throw new ParameterException("当前日期不能为空");
+    }
+    if (currentDate.isBeforeOrEqual(birthday)) {
+      return 0;
+    }
+
+    DateTime birthdayInnerTime = birthday.getInnerTime();
+    DateTime currentDateInnerTime = currentDate.getInnerTime();
+
+    int yearsBetweenTwoDate = Years.yearsBetween(birthdayInnerTime, currentDateInnerTime).getYears();
+
+    if (Year.isLeap(birthdayInnerTime.getYear()) && birthdayInnerTime.getMonthOfYear() == 2 && birthdayInnerTime.getDayOfMonth() == 29 &&
+        !Year.isLeap(currentDateInnerTime.getYear()) && currentDateInnerTime.getMonthOfYear() == 2 && currentDateInnerTime.getDayOfMonth() == 28) {
+      // 公民出生日期为闰年2月29日的，在计算周岁时，遇到非闰年以3月1日为满周岁；如某公民出生日期为1992年2月29日，该公民满18周岁的日期为2010年3月1日。
+      // yearsBetweenTwoDate 计算闰年和非闰年之间的年数时，会认为 1992-02-29 与 1993-02-28 之间有一整年（把 02-29 与非闰年的 02-28 视作等价），因此这里特殊处理减掉一年
+      return yearsBetweenTwoDate - 1;
+    }
+    else {
+      return yearsBetweenTwoDate;
+    }
   }
 
   /**
