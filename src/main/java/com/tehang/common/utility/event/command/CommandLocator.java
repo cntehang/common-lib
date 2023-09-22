0ca2188a3dbab5694053fda742e920857e244f5a
haoyuhai <haoyuhai@163.com>
2023-09-22 17:25:59 +0800

    优化命令查询逻辑

--- src/main/java/com/tehang/common/utility/event/command/CommandLocator.java
+++ src/main/java/com/tehang/common/utility/event/command/CommandLocator.java
@@ -4,6 +4,10 @@ import com.tehang.common.infrastructure.exceptions.SystemErrorException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationContext;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
 
 /**
@@ -18,11 +22,25 @@ public class CommandLocator {
    * 根据命令类型查找命令对象。
    */
   public Command findCommandEnsured(String commandType) {
-    return applicationContext.getBeansOfType(Command.class)
+    var commands = applicationContext.getBeansOfType(Command.class)
         .values()
         .stream()
         .filter(item -> equalsIgnoreCase(item.getCommandType(), commandType))
-        .findFirst()
-        .orElseThrow(() -> new SystemErrorException("未找到命令对象, commandType: " + commandType));
+        .collect(Collectors.toList());
+
+    if (isEmpty(commands)) {
+      throw new SystemErrorException("未找到命令对象, commandType: " + commandType);
+    }
+    if (commands.size() > 1) {
+      throw new SystemErrorException(String.format("找到多个命令对象, commandType: %s, commandNames: %s", commandType, getCommandNames(commands)));
+    }
+
+    return commands.get(0);
+  }
+
+  private static String getCommandNames(List<Command> commands) {
+    return commands.stream()
+        .map(item -> item.getClass().getSimpleName())
+        .collect(Collectors.joining(","));
   }
 }
