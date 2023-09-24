56ffc6a708006904c432ad5ed443fa1fcc48cf22
haoyuhai <haoyuhai@163.com>
2023-09-24 18:14:38 +0800

    save2

--- src/main/java/com/tehang/common/utility/event/command/CommandArgsTypeParser.java
+++ src/main/java/com/tehang/common/utility/event/command/CommandArgsTypeParser.java
@@ -14,18 +14,65 @@ public final class CommandArgsTypeParser {
   }
 
   public static CommandTypeInfo parse(Command command) {
-    // 获取command的直接实现接口
-    Type[] genericInterfaces = command.getClass().getGenericInterfaces();
+    // 递归查找command对象及其基类实现的Command接口参数
+    CommandTypeInfo commandTypeInfo = getCommandTypeInfo(command);
+    if (commandTypeInfo == null) {
+      throw new SystemErrorException("invalid Command Type: " + command.getClass());
+    }
+    return commandTypeInfo;
+  }
 
-    for (Type genericInterface : genericInterfaces) {
-      if (genericInterface instanceof ParameterizedType) {
-        ParameterizedType parameterizedType = (ParameterizedType) genericInterface;
-        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+  private static CommandTypeInfo getCommandTypeInfo(Object obj) {
+    Class<?> clazz = obj.getClass();
+    while (clazz != null) {
+      // 获取clazz直接实现的接口列表
+      Type[] genericInterfaces = clazz.getGenericInterfaces();
 
-        return CommandTypeInfo.of(actualTypeArguments[0].getClass(), actualTypeArguments[1].getClass());
+      for (Type genericInterface : genericInterfaces) {
+        if (genericInterface instanceof ParameterizedType) {
+          var parameterizedType = (ParameterizedType) genericInterface;
+          if (parameterizedType.getRawType() == Command.class) {
+            // 找到该clazz实现的Command泛型接口
+            return getCommandTypeInfo(parameterizedType);
+          }
+        }
       }
+      // 继续查找基类的接口
+      clazz = clazz.getSuperclass();
+    }
+    return null;
+  }
+
+  private static CommandTypeInfo getCommandTypeInfo(ParameterizedType commandParameterizedType) {
+    Type[] actualTypeArguments = commandParameterizedType.getActualTypeArguments();
+    return CommandTypeInfo.of(
+        (Class<?>) actualTypeArguments[0],
+        (Class<?>) actualTypeArguments[1]
+    );
+  }
+
+  public static void main(String[] args) {
+    var typeInfo = parse(new TestCommandBase());
+    System.out.println(typeInfo.getArgsClass());
+    System.out.println(typeInfo.getReturnClass());
+    System.out.println(typeInfo);
+  }
+
+  static class TestCommandBase implements Command<String, Void> {
+
+    @Override
+    public String getCommandType() {
+      return "MyTestCommand";
     }
 
-    throw new SystemErrorException("invalid Command Type: " + command.getClass());
+    @Override
+    public Void run(String args) {
+      System.out.println(args);
+      return null;
+    }
+  }
+
+  static class TestCommand extends TestCommandBase {
+
   }
 }
