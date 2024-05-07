9534375939d033f197f38cf3f8ddf715976298ed
haoyuhai <haoyuhai@163.com>
2024-05-07 17:32:06 +0800

    添加金额分摊的辅助类 (#112)
    * 添加金额分摊的辅助类
    
    * fix pmd
    
    * update submodule

---
+++ src/main/java/com/tehang/common/utility/money/AmountApportionment.java
@@ -0,0 +1,155 @@
+package com.tehang.common.utility.money;
+
+import com.tehang.common.utility.BigDecimalUtils;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.tehang.common.utility.CollectionUtils.isEmpty;
+import static java.util.stream.Collectors.toList;
+
+/** 金额分摊的辅助类。*/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class AmountApportionment {
+
+  private static final BigDecimal HUNDRED = new BigDecimal(100);
+
+  /**
+   * 金额分摊算法。按指定的比例进行分摊，返回分摊后的金额列表。
+   * @param totalAmount 待分摊的总金额，不能为null, 也不能为负数
+   * @param ratios 分摊的系数列表，不能为空，也不能为负数
+   * @return 分摊后的金额列表
+   */
+  public static List<Money> apportion(Money totalAmount, List<Money> ratios) {
+    if (totalAmount == null || totalAmount.lessThan(Money.ZERO)) {
+      throw new IllegalArgumentException("无效的totalAmount: " + totalAmount);
+    }
+
+    if (isEmpty(ratios)) {
+      throw new IllegalArgumentException("ratios不能为空");
+    }
+
+    for (Money ratio : ratios) {
+      if (ratio == null || ratio.lessThan(Money.ZERO)) {
+        throw new IllegalArgumentException("无效的ratio值: " + ratio);
+      }
+    }
+
+    // 金额拆分：转换为long类型进行拆分
+    var longResults = apportion(toCents(totalAmount.getAmount()), getLongRatiosForMoney(ratios));
+
+    // 将拆分后的long类型金额转换为Money类型
+    return longResults.stream()
+        .map(Money::parseByCents)
+        .collect(toList());
+  }
+
+  /**
+   * 金额分摊算法。按指定的比例进行分摊，返回分摊后的金额列表。
+   * @param totalAmount 待分摊的总金额，不能为null, 也不能为负数
+   * @param ratios 分摊的系数列表，不能为空，也不能为负数
+   * @return 分摊后的金额列表
+   */
+  public static List<BigDecimal> apportion(BigDecimal totalAmount, List<BigDecimal> ratios) {
+    if (totalAmount == null || BigDecimalUtils.lessThanZero(totalAmount)) {
+      throw new IllegalArgumentException("无效的totalAmount: " + totalAmount);
+    }
+
+    if (isEmpty(ratios)) {
+      throw new IllegalArgumentException("ratios不能为空");
+    }
+
+    for (BigDecimal ratio : ratios) {
+      if (ratio == null || BigDecimalUtils.lessThanZero(ratio)) {
+        throw new IllegalArgumentException("无效的ratio值: " + ratio);
+      }
+    }
+
+    // 金额拆分：转换为long类型进行拆分
+    var longResults = apportion(toCents(totalAmount), getLongRatiosForDecimal(ratios));
+
+    // 将拆分后的long类型金额转换为BigDecimal类型
+    return longResults.stream()
+        .map(item -> new BigDecimal(item).divide(HUNDRED, 2, RoundingMode.HALF_UP))
+        .collect(toList());
+  }
+
+  /**
+   * 金额分摊算法。按指定的比例进行分摊，返回分摊后的金额列表。
+   * @param totalAmount 待分摊的总金额，不能为负数
+   * @param ratios 分摊的系数列表，不能为空，也不能为负数
+   * @return 分摊后的金额列表
+   */
+  public static List<Long> apportion(long totalAmount, List<Long> ratios) {
+    if (totalAmount < 0) {
+      throw new IllegalArgumentException("totalAmount不能为负数");
+    }
+    if (isEmpty(ratios)) {
+      throw new IllegalArgumentException("ratios不能为空");
+    }
+
+    for (Long ratio : ratios) {
+      if (ratio == null || ratio < 0) {
+        throw new IllegalArgumentException("无效的ratio值: " + ratio);
+      }
+    }
+
+    // 计算总的分摊比例
+    long sumRatio = 0;
+    for (Long ratio : ratios) {
+      sumRatio += ratio;
+    }
+
+    // 创建一个用于存放分摊金额的列表
+    List<Long> apportionedAmounts = new ArrayList<>();
+
+    // 分摊金额, 并累加已分摊的总金额
+    long totalApportionedAmount = 0;
+    for (long ratio : ratios) {
+      // 计算分摊金额（以分为单位）
+      long apportionedAmount;
+
+      if (sumRatio == 0) {
+        // 如果分摊系数全为0，则平均分摊
+        apportionedAmount = totalAmount / ratios.size();
+      }
+      else {
+        apportionedAmount = totalAmount * ratio / sumRatio;
+      }
+      apportionedAmounts.add(apportionedAmount);
+      totalApportionedAmount += apportionedAmount;
+    }
+
+    // 分摊可能会造成精度损失，将差值添加到最后一个分摊金额中
+    long adjustment = totalAmount - totalApportionedAmount;
+    if (adjustment != 0) {
+      // 对最后一个分摊金额进行调整
+      apportionedAmounts.set(apportionedAmounts.size() - 1 , apportionedAmounts.get(apportionedAmounts.size() - 1) + adjustment);
+    }
+
+    return apportionedAmounts;
+  }
+
+  private static List<Long> getLongRatiosForDecimal(List<BigDecimal> ratios) {
+    return ratios.stream()
+        .map(AmountApportionment::toCents)
+        .collect(toList());
+  }
+
+  private static List<Long> getLongRatiosForMoney(List<Money> ratios) {
+    return ratios.stream()
+        .map(item -> toCents(item.getAmount()))
+        .collect(toList());
+  }
+
+  /** 将货币转换为分。即乘以100，然后四舍五入取整。*/
+  private static Long toCents(BigDecimal decimal) {
+    return decimal.multiply(HUNDRED)
+        .setScale(0, RoundingMode.HALF_UP)
+        .longValue();
+  }
+}
---
+++ src/test/groovy/com/tehang/common/utility/money/AmountApportionmentSpec.groovy
@@ -0,0 +1,89 @@
+package com.tehang.common.utility.money
+
+import com.tehang.common.TestSpecification
+
+/** 金额分摊的测试类 */
+class AmountApportionmentSpec extends TestSpecification {
+
+  def "test1: AmountApportionment.apportion for Money"() {
+    when:
+    Money totalAmount = new Money(10)
+    Money ratio1 = new Money(1)
+    Money ratio2 = new Money(1)
+    Money ratio3 = new Money(1)
+
+    List<Money> result = AmountApportionment.apportion(totalAmount, List.of(ratio1, ratio2, ratio3))
+
+    then:
+    result.size() == 3
+    result[0] == new Money(3.33)
+    result[1] == new Money(3.33)
+    result[2] == new Money(3.34)
+  }
+
+  def "test1: AmountApportionment.apportion with zero ratios"() {
+    when:
+    Money totalAmount = new Money(900)
+    Money ratio1 = new Money(0)
+    Money ratio2 = new Money(0)
+    Money ratio3 = new Money(0)
+
+    List<Money> result = AmountApportionment.apportion(totalAmount, List.of(ratio1, ratio2, ratio3))
+
+    then:
+    result.size() == 3
+    result[0] == new Money(300)
+    result[1] == new Money(300)
+    result[2] == new Money(300)
+  }
+
+  def "test1.1: AmountApportionment.apportion with different ratios"() {
+    when:
+    Money totalAmount = new Money(900)
+    Money ratio1 = new Money(0)
+    Money ratio2 = new Money(2)
+    Money ratio3 = new Money(3)
+    Money ratio4 = new Money(4)
+
+    List<Money> result = AmountApportionment.apportion(totalAmount, List.of(ratio1, ratio2, ratio3, ratio4))
+
+    then:
+    result.size() == 4
+    result[0] == new Money(0)
+    result[1] == new Money(200)
+    result[2] == new Money(300)
+    result[3] == new Money(400)
+  }
+
+  def "test2: AmountApportionment.apportion for BigDecimal"() {
+    when:
+    BigDecimal totalAmount = new BigDecimal("10")
+    BigDecimal ratio1 = new BigDecimal("1")
+    BigDecimal ratio2 = new BigDecimal("1")
+    BigDecimal ratio3 = new BigDecimal("1")
+
+    List<BigDecimal> result = AmountApportionment.apportion(totalAmount, List.of(ratio1, ratio2, ratio3))
+
+    then:
+    result.size() == 3
+    result[0] == new BigDecimal("3.33")
+    result[1] == new BigDecimal("3.33")
+    result[2] == new BigDecimal("3.34")
+  }
+
+  def "test3: AmountApportionment.apportion for Long"() {
+    when:
+    long totalAmount = 1000
+    long ratio1 = 1
+    long ratio2 = 1
+    long ratio3 = 1
+
+    List<Long> result = AmountApportionment.apportion(totalAmount, List.of(ratio1, ratio2, ratio3))
+
+    then:
+    result.size() == 3
+    result[0] == 333
+    result[1] == 333
+    result[2] == 334
+  }
+}
