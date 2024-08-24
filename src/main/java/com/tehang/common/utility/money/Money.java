53b60a627801ccb350dace98f331d3e42cabcb0d
haoyuhai <haoyuhai@163.com>
2024-08-24 10:16:08 +0800

    添加Money对象是否大于0，小于0的方法

--- src/main/java/com/tehang/common/utility/money/Money.java
+++ src/main/java/com/tehang/common/utility/money/Money.java
@@ -228,6 +228,11 @@ public class Money implements Serializable, Comparable<Money> {
     return this.compareTo(o) > 0;
   }
 
+  /** 当前对象是否大于0 */
+  public boolean greaterThanZero() {
+    return this.greaterThan(Money.ZERO);
+  }
+
   /** 当前对象是否大于或等于指定的值 */
   public boolean greaterThanOrEqual(@NotNull Money o) {
     return this.compareTo(o) >= 0;
@@ -236,6 +241,11 @@ public class Money implements Serializable, Comparable<Money> {
   /** 当前对象是否小于指定的值 */
   public boolean lessThan(@NotNull Money o) {
     return this.compareTo(o) < 0;
+  }
+
+  /** 当前对象是否小于0 */
+  public boolean lessThanZero() {
+    return this.lessThan(Money.ZERO);
   }
 
   /** 当前对象是否小于或等于指定的值 */
